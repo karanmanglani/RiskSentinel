03c2e723d60b6ed5552e40b96b5818dea50ae7af
karanmanglani <bkn45for@gmail.com>
2026-02-04 11:38:38 +0530

    minimal changes

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup
+
+## To run backend
+
+-- Activate Virtual Environment
+-- go to backend directory and use this command "uvicorn app.main:app --reload"
+-- for frontend go to its directory and run command "npm run dev"
--- backend/app/auth.py
+++ backend/app/auth.py
@@ -30,9 +30,9 @@ def get_password_hash(password):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(datetime.timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=15)
     
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
